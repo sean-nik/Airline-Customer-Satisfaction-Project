1b7ded9f12d7e8f7f4edb5662de387d18cf96139
jtoepp <52012857+jtoepp@users.noreply.github.com>
2020-04-27 14:59:50 -0500

    added require() to test library

--- scripts/AirlineDataAnalysis.R
+++ scripts/AirlineDataAnalysis.R
@@ -1,4 +1,6 @@
 # run install.packages("readxl") if not already installed
+# replaced the install.packages("readxl") with require
+require("readxl")
 library("readxl")
 raw_data <- read_excel("./data/SatisfactionSurvey.xlsx")
 
