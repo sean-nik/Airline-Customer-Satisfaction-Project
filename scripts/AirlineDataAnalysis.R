bb065a2950fd762403e57a5121cc8086d75adcc0
jtoepp <52012857+jtoepp@users.noreply.github.com>
2020-04-27 20:42:35 -0500

    removed require() function after reading about cons

--- scripts/AirlineDataAnalysis.R
+++ scripts/AirlineDataAnalysis.R
@@ -1,6 +1,4 @@
 # run install.packages("readxl") if not already installed
-# replaced the install.packages("readxl") with require
-require("readxl")
 library("readxl")
 raw_data <- read_excel("./data/SatisfactionSurvey.xlsx")
 
