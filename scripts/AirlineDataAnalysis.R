861fb7f1abc1d2ac439b91eae2da8751c580be50
jtoepp <52012857+jtoepp@users.noreply.github.com>
2020-05-09 23:01:06 -0500

    adding  histograms and descriptive statistics function

--- scripts/AirlineDataAnalysis.R
+++ scripts/AirlineDataAnalysis.R
@@ -6,10 +6,10 @@ library(tidyr)
 library(reshape2)
 library(grDevices)
 library(readxl)
-install.packages("naniar")
+# install.packages("naniar")
 library(naniar)
 library(visdat)
-install.packages("ggcharts")
+# install.packages("ggcharts")
 library(ggcharts)
 
 # read in the data
@@ -32,18 +32,30 @@ na_test <- raw_data[c(1, 23, 24, 26)]
 str(na_test)
 
 # visualize how much data is missing
+# seems to work more consistently with its default theme
 vis_miss(na_test)
 
 # find how many elements are missing from each and their relationships
+# does not work with theme_dark() or labs()
 gg_miss_upset(na_test)
 
 # count number of missing elements from each variable
-gg_miss_var(na_test)
+gg_miss_var(na_test) + 
+  theme_dark() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Number of NA's per Variable")
 
 # discover which ratings the most missing data is from 
-gg_miss_var(na_test, facet = Satisfaction)
+gg_miss_var(na_test, facet = Satisfaction) + 
+  theme_dark() +
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5)) +
+  labs(title = "Number of NA's per Variable",
+       subtitle = "Grouped by Satisfaction Score")
 
+# replace NA's with column means to preserve the rest of the data
+df <- data.frame(na_mean(raw_data, option = "median"))
 
-
-
+# verify no NA's remain
+any(is.na(df))
 
